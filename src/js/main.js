f9a894ef2c45a4eee9b7ec66e015b43c89dedbcf
leny <info@flatland.be>
2016-10-06 00:33:58 +0200

    Refactor code to ES2016 & made tracks clickable

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,83 @@
-( function() {
+/* leny/meguiddo
+ *
+ * /main.js - Main page script
+ *
+ * coded by leny@flatLand!
+ * started at 06/10/2016 (es2016 rewriting)
+ */
 
-    var oAudio,
-        iCurrentTrackIndex = -1,
-        fChangeTrack,
-        $playPauseButton,
-        aTracks = [
-            "assets/audio/meguiddo-01-black-sea.mp3",
-            "assets/audio/meguiddo-02-world.mp3",
-            "assets/audio/meguiddo-03-stardust.mp3"
-        ];
+/* eslint-disable no-invalid-this */
 
-    if ( !window.Audio ) {
-        document.querySelector( ".controls" ).remove();
-        return;
+const TRACKS = [
+    "assets/audio/meguiddo-01-black-sea.mp3",
+    "assets/audio/meguiddo-02-world.mp3",
+    "assets/audio/meguiddo-03-stardust.mp3",
+];
+
+let oAudio,
+    iCurrentTrackIndex = -1,
+    fChangeTrack, fPlayTrack,
+    $playPauseButton;
+
+if ( !window.Audio ) {
+    document.querySelector( ".controls" ).remove();
+
+    throw new Error( "Audio isn't supported!" );
+}
+
+( oAudio = new Audio() ).addEventListener( "ended", () => fChangeTrack() );
+
+fPlayTrack = function( iTrackNumber = 0 ) {
+    let $ref;
+
+    oAudio.pause();
+    oAudio.src = TRACKS[ iTrackNumber ];
+    oAudio.play();
+    $playPauseButton.classList.remove( "play" );
+    $playPauseButton.classList.add( "pause" );
+
+    ( $ref = document.querySelector( "li.active" ) ) && $ref.classList.remove( "active" );
+    document.querySelectorAll( "li" )[ iCurrentTrackIndex ].classList.add( "active" );
+};
+
+fChangeTrack = function( bPrev ) {
+    if ( bPrev ) {
+        ( --iCurrentTrackIndex < 0 ) && ( iCurrentTrackIndex = TRACKS.length - 1 );
+    } else {
+        ( ++iCurrentTrackIndex === TRACKS.length ) && ( iCurrentTrackIndex = 0 );
     }
 
-    oAudio = new Audio();
-    oAudio.addEventListener( "ended", function() {
-        fChangeTrack();
-    } );
+    fPlayTrack( iCurrentTrackIndex );
+};
 
-    fChangeTrack = function( bPrev ) {
-        var _ref;
-
-        if ( bPrev ) {
-            ( --iCurrentTrackIndex < 0 ) && ( iCurrentTrackIndex = aTracks.length - 1 );
-        } else {
-            ( ++iCurrentTrackIndex === aTracks.length ) && ( iCurrentTrackIndex = 0 );
-        }
-
-        oAudio.pause();
-        oAudio.src = aTracks[ iCurrentTrackIndex ];
-        oAudio.play();
-        $playPauseButton.classList.remove( "play" );
-        $playPauseButton.classList.add( "pause" );
-        ( _ref = document.querySelector( "li.active" ) ) && _ref.classList.remove( "active" );
-        document.querySelectorAll( "li" )[ iCurrentTrackIndex ].classList.add( "active" );
-    };
-
-    ( $playPauseButton = document.querySelector( ".play-pause" ) ).addEventListener( "click", function( e ) {
+Array.from( document.querySelectorAll( "li" ) ).forEach( ( $elt, iIndex ) => {
+    $elt.addEventListener( "click", ( e ) => {
         e.preventDefault();
-
-        if ( !oAudio.src ) {
-            return fChangeTrack();
-        }
-
-        oAudio[ this.classList.contains( "pause" ) ? "pause" : "play" ]();
-
-        this.classList.toggle( "pause" );
-        this.classList.toggle( "play" );
+        fPlayTrack( iCurrentTrackIndex = iIndex );
     } );
+} );
 
-    document.querySelector( ".prev" ).addEventListener( "click", function( e ) {
-        e.preventDefault();
-        fChangeTrack( true );
-    } );
+( $playPauseButton = document.querySelector( ".play-pause" ) ).addEventListener( "click", function( e ) {
+    e.preventDefault();
 
-    document.querySelector( ".next" ).addEventListener( "click", function( e ) {
-        e.preventDefault();
-        fChangeTrack();
-    } );
+    if ( !oAudio.src ) {
+        return fChangeTrack();
+    }
 
-} )();
+    oAudio[ this.classList.contains( "pause" ) ? "pause" : "play" ]();
+
+    this.classList.toggle( "pause" );
+    this.classList.toggle( "play" );
+
+    return null;
+} );
+
+document.querySelector( ".prev" ).addEventListener( "click", ( e ) => {
+    e.preventDefault();
+    fChangeTrack( true );
+} );
+
+document.querySelector( ".next" ).addEventListener( "click", ( e ) => {
+    e.preventDefault();
+    fChangeTrack();
+} );
